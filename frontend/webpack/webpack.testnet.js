4daaded1b40b4c3ccac255d77a3283b3fdacba3c
hummingalpaca <ricky@mobilecoin.com>
2023-01-31 08:22:45 -0800

    test with our URLs

--- frontend/webpack/webpack.testnet.js
+++ frontend/webpack/webpack.testnet.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [
     // new BundleAnalyzerPlugin(),
     new DefinePlugin({
-      AUDITOR_URL: JSON.stringify('https://auditor.eusd.one/testnet/api'),
+      AUDITOR_URL: JSON.stringify('https://reserve-auditor.test.mobilecoin.com/api/'),
       GNOSIS_SAFE_API_URL: JSON.stringify('https://safe-transaction.goerli.gnosis.io'),
       ETHERSCAN_URL: JSON.stringify('https://goerli.etherscan.io'),
     })

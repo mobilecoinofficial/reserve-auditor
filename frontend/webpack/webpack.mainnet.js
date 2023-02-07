c32a914188014b71c00506d9bbea16554d17d02d
hummingalpaca <ricky@mobilecoin.com>
2023-02-06 16:44:01 -0800

    fix: update urls

--- frontend/webpack/webpack.mainnet.js
+++ frontend/webpack/webpack.mainnet.js
@@ -4,7 +4,7 @@ module.exports = {
   devtool: 'source-map',
   plugins: [
     new DefinePlugin({
-      AUDITOR_URL: JSON.stringify('https://reserve-auditor.prod.mobilecoin.com/api/'),
+      AUDITOR_URL: JSON.stringify('https://auditor.mobilecoin.foundation/api/'),
       GNOSIS_SAFE_API_URL: JSON.stringify('https://safe-transaction-mainnet.safe.global'),
       ETHERSCAN_URL: JSON.stringify('https://etherscan.io'),
     })

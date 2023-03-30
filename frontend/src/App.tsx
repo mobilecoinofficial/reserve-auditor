bb4f2bc46137cd66579ae9a13faab54bf08e0c54
Colin Carey <colin.carey1@gmail.com>
2023-03-30 08:49:16 -0700

    Remove old files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 
 import theme from './theme'
 import { GnosisSafeProvider } from './contexts'
-import Layout from './layout/Layout'
+import Layout from './components/Layout'
 
 export const App = () => {
   return (
--- frontend/src/components/BottomContent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function BottomContent() {
-  return (
-    <div style={{ height: 600, border: '1px solid black', padding: 16 }}>
-      Bottom Content gos here
-    </div>
-  )
-}

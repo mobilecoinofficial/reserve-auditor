9378d62bb440c9428b5e93796f99beb194d02bef
Colin Carey <colin.carey1@gmail.com>
2023-04-03 14:14:30 -0700

    move dbs out of tmp

--- scripts/start-http-server.sh
+++ scripts/start-http-server.sh
@@ -3,4 +3,4 @@ MC_LOG=mc_reserve_auditor=info cargo run --release \
     -p mc-reserve-auditor -- \
     start-http-server \
     --gnosis-safe-config ../gnosis-safe.json \
-    --reserve-auditor-db /tmp/auditor-db \
+    --reserve-auditor-db ~/Documents/data/auditor-db \

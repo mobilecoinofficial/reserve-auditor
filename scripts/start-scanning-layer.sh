d41d07ed8b5c3c7c65924c2628a506477305de85
Colin Carey <colin.carey1@gmail.com>
2023-04-03 14:14:30 -0700

    move dbs out of tmp

--- scripts/start-scanning-layer.sh
+++ scripts/start-scanning-layer.sh
@@ -3,6 +3,6 @@ MC_LOG=mc_reserve_auditor=info cargo run --release \
     -p mc-reserve-auditor -- \
     scan-ledger \
     --gnosis-safe-config ../gnosis-safe.json \
-    --ledger-db /tmp/ledger-db \
-    --reserve-auditor-db /tmp/auditor-db \
-    --watcher-db /tmp/watcher-db/ \
+    --ledger-db ~/Documents/data/ledger-db \
+    --reserve-auditor-db ~/Documents/data/auditor-db \
+    --watcher-db ~/Documents/data/watcher-db/ \

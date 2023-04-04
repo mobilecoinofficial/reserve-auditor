feba6794cc2ae2a28a3779ab3592303a724d8555
Colin Carey <colin.carey1@gmail.com>
2023-04-04 08:33:08 -0700

    address feedback

--- frontend/src/components/CopyableField.tsx
+++ frontend/src/components/CopyableField.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
-import { Typography, Box, Tooltip, IconButton } from '@mui/material'
+import { Typography, Box, Tooltip, IconButton, Link } from '@mui/material'
+import LaunchIcon from '@mui/icons-material/Launch'
 
 import { abbreviateHash } from '../utils/truncate'
 
 export default function CopyableField({
   text,
   abbreviate = true,
+  link,
+  copy = true,
 }: {
   text: string
   abbreviate?: boolean
+  link: string
+  copy?: boolean
 }) {
   function copyToClipboard(e: React.MouseEvent<HTMLButtonElement>) {
     e.stopPropagation()
@@ -20,19 +25,43 @@ export default function CopyableField({
 
   return (
     <Box display="flex" alignItems="center">
-      <Tooltip title={text}>
-        <IconButton
-          onClick={copyToClipboard}
-          edge="start"
-          sx={{ paddingLeft: '12px' }}
-        >
-          <ContentCopyIcon
-            sx={{ cursor: 'pointer', color: 'text.secondary' }}
-            fontSize="small"
-          />
-        </IconButton>
-      </Tooltip>
-      <Typography sx={{ fontSize: 14 }}>{renderedText}</Typography>
+      <Typography sx={{ fontSize: 14, marginRight: 1 }}>
+        {renderedText}
+      </Typography>
+      {copy && (
+        <Tooltip title="Copy to clipboard">
+          <IconButton onClick={copyToClipboard} edge="start" size="small">
+            <ContentCopyIcon
+              sx={{
+                cursor: 'pointer',
+                color: 'text.secondary',
+                height: 16,
+                width: 16,
+              }}
+            />
+          </IconButton>
+        </Tooltip>
+      )}
+      {link && (
+        <Tooltip title="Go to blockchain">
+          <Link href={link} target="_blank" rel="noreferrer">
+            <IconButton
+              onClick={(e) => e.stopPropagation()}
+              edge="start"
+              size="small"
+            >
+              <LaunchIcon
+                sx={{
+                  cursor: 'pointer',
+                  color: 'text.secondary',
+                  height: 16,
+                  width: 16,
+                }}
+              />
+            </IconButton>
+          </Link>
+        </Tooltip>
+      )}
     </Box>
   )
 }
--- frontend/src/utils/truncate.ts
+++ frontend/src/utils/truncate.ts
@@ -3,7 +3,7 @@ export function truncate(str: string, n: number) {
 }
 
 export function abbreviateHash(hash: string) {
-  const start = hash.slice(0, 5)
-  const end = hash.slice(hash.length - 5, hash.length)
+  const start = hash.slice(0, 7)
+  const end = hash.slice(hash.length - 7, hash.length)
   return start + '...' + end
 }

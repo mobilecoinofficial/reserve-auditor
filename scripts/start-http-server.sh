c277676f129729f32e0dacf5ef3240cf24709d80
Colin Carey <colin.carey1@gmail.com>
2023-04-04 12:05:36 -0700

    Fix scripts

--- scripts/start-http-server.sh
+++ scripts/start-http-server.sh
@@ -1,4 +1,4 @@
-CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css \
+SGX_MODE=HW IAS_MODE=PROD CONSENSUS_ENCLAVE_CSS=$(pwd)/../mobilecoin/consensus-enclave.css \
 MC_LOG=mc_reserve_auditor=info cargo run --release \
     -p mc-reserve-auditor -- \
     start-http-server \

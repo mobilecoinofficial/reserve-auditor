c97d1ec8e3b94d58c944f57567f65ee78d49e804
Colin Carey <colin.carey1@gmail.com>
2023-04-10 17:27:43 -0700

    Add staging configs

---
+++ frontend/webpack/webpack.staging-test.js
@@ -0,0 +1,19 @@
+const { DefinePlugin } = require('webpack')
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  devtool: 'source-map',
+  plugins: [
+    // new BundleAnalyzerPlugin(),
+    new DefinePlugin({
+      AUDITOR_URL: JSON.stringify(
+        'https://auditor.stage.test.mobilecoin.com/api/'
+      ),
+      GNOSIS_SAFE_API_URL: JSON.stringify(
+        'https://safe-transaction-goerli.safe.global'
+      ),
+      ETHERSCAN_URL: JSON.stringify('https://goerli.etherscan.io'),
+      MC_NETWORK: JSON.stringify('testnet'),
+    }),
+  ],
+}

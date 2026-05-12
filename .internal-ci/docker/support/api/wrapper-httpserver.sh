9ad739225158e0d6939e0f3276f2c9907e6b55a4
Joe Kottke <joe.kottke@gmail.com>
2026-05-12 14:30:57 -0500

    Reserve auditor cleanup (#62)
    * Set mdb file paths to standard locations, prepare for ledger download
    * Make entrypoint exit immediate if anything fails
    * Move httpserver to main container, cleanup sts yaml
    * Cleanup some whitespace
    * Missed cpoypasta of httpserver supervisor filename

---
+++ .internal-ci/docker/support/api/wrapper-httpserver.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Wrapper script to watch for when scanner creates the auditor.db and then start the
+# http server.
+
+# First wait for the file to be created (should be created before anything starts)
+while test ! -f /data/auditor/auditor.db
+do
+    echo "Wating for scanner to create /data/auditor/auditor.db"
+    sleep 2
+done
+
+# Now wait for it to be a non-empty file. SQLite DB will be non-empty once created.
+while test ! -s /data/auditor/auditor.db
+do
+    echo "Waiting for scanner to create SQLite database in /data/auditor/auditor.db"
+    sleep 2
+done
+
+# Now we can start the http-server
+/usr/local/bin/mc-reserve-auditor \
+    start-http-server \
+    --host 0.0.0.0 \
+    --reserve-auditor-db /data/auditor/auditor.db \
+    --gnosis-safe-config /config/gnosis/config.json
